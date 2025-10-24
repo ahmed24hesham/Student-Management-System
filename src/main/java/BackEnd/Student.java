124111ecb77bfbb783ae83d6461f2e30e54ebde3
ahmedelsayed67 <ahmedelsayed200427@gmail.com>
2025-10-24 20:42:59 +0300

    First Class Student

--- src/main/java/BackEnd/Student.java
+++ src/main/java/BackEnd/Student.java
@@ -1,5 +1,99 @@
 package BackEnd;
-
+import java.util.Random;
 public class Student {
+private int StudentID;
+ private String FullName;
+private int Age;
+private String Gender;
+private String Department;
+ private double GPA;
+ public Student(int StudentID,String FullName,int Age,String Gender,String Department ,double GPA){
+     if(9000<=StudentID&&StudentID<10000){
+     this.StudentID=StudentID;
+     }
+     else{
+         int randomnumber=getRandomNumber(9000,10000);
+           this.StudentID=randomnumber;
+     }
+     if(checkerFullName(FullName)){
+     this.FullName=FullName;
+     }
+     else{
+         System.out.println("Something Wrong in FullName"); 
+     }
+     
+     if(Age>0){
+     this.Age=Age;
+     }
+     else{
+     System.out.println("Age is't True"); 
+     }
+     if(Gender=="Male"||Gender=="Female"){
+       this.Gender=Gender;  
+     }
+     else{
+      System.out.println("Gender is't True");
+     }
+     this.Department=Department;
+     if(0<=GPA&&GPA<=4){
+    this.GPA=GPA;
+     }
+     else{
+      System.out.println("GPA is't True");
+     }
+ } 
+    public int getStudentID() {
+        return StudentID;
+    }
 
+    public void setStudentID(int StudentID) {
+        this.StudentID = StudentID;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String FullName) {
+        this.FullName = FullName;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    } public String getDepartment() {
+        return Department;
+    }
+    public void setDepartment(String Department) {
+        this.Department = Department;
+    }
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }   
+public static int getRandomNumber(int min,int max){
+Random rand=new Random();
+return rand.nextInt(max-min+1)+min;
+}
+public boolean checkerFullName(String FullName){
+for(int i=0;i<FullName.length();i++){ 
+    if(Character.isDigit(FullName.charAt(i))){
+        return false;
+    }
+}
+return true;
+}
 }
--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -1,5 +1,15 @@
 package Main;
 
-public class main {
+import BackEnd.Student;
 
+public class main {
+   public static void main(String args[]){
+    Student kk=new Student(10555,"Ahmed Elsa",25,"Male","lol",4);
+    System.out.println(kk.getAge());
+     System.out.println(kk.getFullName());
+      System.out.println(kk.getGPA());
+       System.out.println(kk.getStudentID());
+        System.out.println(kk.getGender());
+
+}
 }
